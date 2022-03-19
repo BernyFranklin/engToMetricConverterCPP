a0508e44123152472a02e53e7b82a048c8ea6a42
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-18 18:16:58 -0700

    Added 2 sub routines
    Started Main loop.
    Used a sub routine for each conversion. And made a subroutine for error checking. Made a constant for the conversion rate.

--- src/englishToMetricConversionCPP/englishToMetricConversionCPP/main.cpp
+++ src/englishToMetricConversionCPP/englishToMetricConversionCPP/main.cpp
@@ -13,18 +13,29 @@
 #include <cctype>
 // Function prototype
 char menuSelect();
+void englishConvert();
+bool isNumber(double input);
 // std namespace
 using namespace std;
+
+// Global Constant conversion rate English to Metric
+const double CONVERSION_RATE = 2.54;
 
 // Start Main
 int main(int argc, const char * argv[]) {
     // Local variables
-    double inches;
-    double centimeters;
     char select;
     
-    
-    
+    // Loop program until user quits
+    do {
+        // Call menuSelect
+        select = menuSelect();
+        // If E selected, convert from English to Metric
+        if (select == 'E') {
+            // Prompt for input
+            englishConvert();
+        }   // End of E selected
+    } while (select != 'Q');
     return 0;
 }   // End Main
 
@@ -46,3 +57,51 @@ char menuSelect() {
     } while (selection != 'E' && selection != 'M' && selection != 'Q');
     return selection;
 }   // End of menuSelect
+
+// Start englishConvert
+void englishConvert() {
+    // Local Variables
+    double inches;
+    double centimeters;
+    bool isValid;
+    // Do..while loop to loop for valid input
+    do {
+        // Assume good input
+        isValid = true;
+        // Prompt for input
+        cout << "Enter the number of inches: ";
+        // Read input
+        cin >> inches;
+        // Check if input is numeric
+        isValid = isNumber(inches);
+        // If not numeric
+        if (!isValid)
+            continue;
+        // If input valid
+        else {
+            // Compute Inches to centimeters
+            centimeters = inches * CONVERSION_RATE;
+            // Print results
+            cout << inches << " inches is equal to " << centimeters << " centimeters." << endl;
+        }   // End of else
+    } while (!isValid);   // Repeat until valid input
+}  // End of englishConvert
+
+// Start of isNumber
+bool isNumber(double input) {
+    // Data mismatch
+    if (!cin) {
+        cout << "Input must be numeric, please try again." << endl;
+        cout << endl; cin.clear();
+        cin.ignore(10000, '\n');
+        return false;
+    }   // End of data mismatch
+    else if (input < 0) {
+        cout << "Input must be positive, please try again." << endl;
+        cout << endl; cin.clear();
+        cin.ignore(10000, '\n');
+        return false;
+    }
+    else;
+    return true;
+}   // End of isNumber
