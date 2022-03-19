4686b016380c87db6e98e5c34cc8c1650fcbdeb7
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-18 17:33:21 -0700

    Added menuSelect function
    Prompts menu selection. Takes input and converts toupper. Loops until proper selection made.

--- src/englishToMetricConversionCPP/englishToMetricConversionCPP/main.cpp
+++ src/englishToMetricConversionCPP/englishToMetricConversionCPP/main.cpp
@@ -27,3 +27,22 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }   // End Main
+
+// Start menuSelect
+char menuSelect() {
+    // Local variable
+    char selection;
+    // Loop menu until a valid input selected E, M, or Q
+    do {
+        // Blank line
+        cout << endl;
+        // Prompt
+        cout << "Enter [E] to convert English to Metric, [M] to convert Metric to English, or [Q] to quit: ";
+        // Read input
+        cin >> selection;
+        // Convert to uppercase
+        selection = toupper(selection);
+        // Repeat loop until E, M, or Q selected
+    } while (selection != 'E' && selection != 'M' && selection != 'Q');
+    return selection;
+}   // End of menuSelect
