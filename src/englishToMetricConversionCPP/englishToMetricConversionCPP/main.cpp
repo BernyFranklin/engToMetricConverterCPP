4b93be61ad087d2cc9672ff12f6b9ae377c0c8b2
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-18 17:23:57 -0700

    Initial commit
    Created header and local variables

---
+++ src/englishToMetricConversionCPP/englishToMetricConversionCPP/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * englishToMetricConversionCPP Version1.0
+ * Frank Bernal
+ * CIS 054 C/C++ Programming
+ * Input: Desired Unit, Input measurement
+ * Output: Conversion Inches <=> Centimeters
+ * 18 Mar 2022
+ */
+
+// Input Output stream
+#include <iostream>
+// toUpper function
+#include <cctype>
+// Function prototype
+char menuSelect();
+// std namespace
+using namespace std;
+
+// Start Main
+int main(int argc, const char * argv[]) {
+    // Local variables
+    double inches;
+    double centimeters;
+    char select;
+    
+    
+    
+    return 0;
+}   // End Main
